5d9aa522aca683150c26e01434525e8ec2810566
abeerdevs <Khatri.abeer1810@gmail.com>
2026-07-25 20:24:17 +0530

    WORKING?!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://nibs-hackathon-2026-production.up.railway.app',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'wss://nibs-hackathon-2026-production.up.railway.app',
+        ws: true,
+        changeOrigin: true,
+      }
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
 })
